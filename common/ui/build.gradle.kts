8663b90b6794237ca7ea865bad39f12050c25771
yusef <yusefpasha8980@gmail.com>
2025-08-27 10:35:39 +0330

    [ADD] Add Compose UI tooling dependency
    This commit adds the `androidx.ui.tooling` dependency to the `common/ui` module. This dependency provides tools for inspecting and debugging Jetpack Compose UIs, and it is configured as a `debugImplementation` to only be included in debug builds.

--- common/ui/build.gradle.kts
+++ common/ui/build.gradle.kts
@@ -28,4 +28,5 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
 }
