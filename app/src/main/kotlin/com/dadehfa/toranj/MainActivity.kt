4c3dd1c49d12146b781b8f207574041338ff6fc2
yusef <yusefpasha8980@gmail.com>
2025-08-27 09:44:53 +0330

    [FEAT] Integrate Splash Screen
    This commit integrates the `SplashScreen` composable into the `MainActivity`.
    
    The `MainActivity` now displays the `SplashScreen` within its `Scaffold`. The previous placeholder `Text` composable has been removed.

--- app/src/main/kotlin/com/dadehfa/toranj/MainActivity.kt
+++ app/src/main/kotlin/com/dadehfa/toranj/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.dadehfa.toranj.common.ui.theme.ToranjTheme
+import com.dadehfa.toranj.features.splash.presentation.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +18,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ToranjTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "Hello Android!",
-                        )
+                    SplashScreen(modifier = Modifier.padding(innerPadding)) {
+
                     }
                 }
             }
