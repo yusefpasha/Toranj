160f0bff42efc60d8dc8e399572f1eddf1012a05
yusef <yusefpasha8980@gmail.com>
2025-08-30 11:02:17 +0330

    [EDIT] Add @Contextual annotation for AuthToken serialization
    This commit adds the `@Contextual` annotation to the `accessToken` and `refreshToken` fields in `LoginResponseDto.kt`. This is necessary for proper serialization/deserialization of the `AuthToken` type when it's not a built-in serializable type.
    
    Additionally, the `kotlin-serialization` plugin has been added to the `features/register/build.gradle.kts` file to enable serialization capabilities for this module.

--- features/register/src/main/kotlin/com/dadehfa/toranj/features/register/data/remote/dto/LoginResponseDto.kt
+++ features/register/src/main/kotlin/com/dadehfa/toranj/features/register/data/remote/dto/LoginResponseDto.kt
@@ -1,17 +1,18 @@
 package com.dadehfa.toranj.features.register.data.remote.dto
 
 import com.dadehfa.toranj.features.register.domain.model.AuthToken
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class LoginResponseDto(
-    val accessToken: AuthToken,
+    val accessToken: @Contextual AuthToken,
     val email: String,
     val firstName: String,
     val gender: String,
     val id: Int,
     val image: String,
     val lastName: String,
-    val refreshToken: AuthToken,
+    val refreshToken: @Contextual AuthToken,
     val username: String
 )
