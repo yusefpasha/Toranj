35163b38908705493d5c7ce69d7ca0423bd26013
yusef <yusefpasha8980@gmail.com>
2025-08-30 11:22:44 +0330

    [EDIT] Clear focus on login button click in RegisterScreen
    This commit updates the `RegisterScreen` to clear the focus from any input field when the login button is clicked.
    
    This is achieved by obtaining an instance of `LocalFocusManager` and calling `focusManager.clearFocus()` within the `onClick` lambda of the login `Button`.

--- features/register/src/main/kotlin/com/dadehfa/toranj/features/register/presentation/RegisterScreen.kt
+++ features/register/src/main/kotlin/com/dadehfa/toranj/features/register/presentation/RegisterScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.dadehfa.toranj.common.ui.component.DefaultEditText
@@ -44,6 +45,7 @@ fun RegisterScreen(
     var username by remember { mutableStateOf("emilys") }
     var password by remember { mutableStateOf("emilyspass") }
     var isRememberMe by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
 
     Scaffold(
         modifier = Modifier
@@ -163,6 +165,7 @@ fun RegisterScreen(
                         )
                         Spacer(modifier = Modifier.height(MaterialTheme.padding.medium))
                         Button(onClick = {
+                            focusManager.clearFocus()
                             onIntent(
                                 RegisterContract.Intent.Login(
                                     username,
